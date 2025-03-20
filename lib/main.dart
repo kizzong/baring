3a5f14f016279b0b6ca79d5fe60cdec222893225
kizzo <89300597+kizzong@users.noreply.github.com>
2025-03-21 00:05:52 +0900

    Add files via upload
    목표 설정, D-day 날짜 설정

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:subme2/list_page.dart';
+
+void main() async {
+  await Hive.initFlutter();
+
+  await Hive.openBox('Goal'); // 목표
+  await Hive.openBox('Date'); // 목표 D-day
+
+  runApp(SubMe());
+}
+
+class SubMe extends StatefulWidget {
+  const SubMe({super.key});
+
+  @override
+  State<SubMe> createState() => _SubMeState();
+}
+
+class _SubMeState extends State<SubMe> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: ListPage(),
+    );
+  }
+}
